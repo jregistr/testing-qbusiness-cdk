2cfd7f54c336d0affbe76ae6c7e53f004b9ab2d2
Jeff Registre <jeregist@amazon.com>
2024-07-19 14:17:59 -0400

    Initial commit

---
+++ test/qbusiness-searchapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QbusinessSearchapp from '../lib/qbusiness-searchapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/qbusiness-searchapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QbusinessSearchapp.QbusinessSearchappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
